d7bedce3f45fc9f4ff9a76ed2ec7b54eb7c0d21f
Netanel Mizrahi <nmiz1987@gmail.com>
2025-04-06 15:59:31 +0300

    Add new SVG icons and images; remove unused SVG files
    - Added logo.svg, logout.svg, receipt.svg, search-fill.svg, star.svg, tick.svg, upload.svg, user-fill.svg, user.svg, verified.svg, and warning.svg to the public/icons directory.
    - Added auth-illustration.png, no-books.png, and pattern.webp to the public/images directory.
    - Removed unused SVG files: next.svg, vercel.svg, and window.svg from the public directory.

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react';
+
+export default function layout({ children }: { children: ReactNode }) {
+  return (
+    <main className="root-container">
+      <div className="mx-auto max-w-7xl">
+        Header
+        <div className="mt-20 pb-20">{children}</div>
+      </div>
+    </main>
+  );
+}
