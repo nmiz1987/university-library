dcbe60e21d0198c0f63af33010de4b3ccb02dbd3
Netanel Mizrahi <nmiz1987@gmail.com>
2025-04-06 17:07:11 +0300

    Add Header, BookList, and BookOverview components! 🎉
    - Implement Header component with navigation links and logo! 🖼️
    - Create BookList component to display a list of books! 📚
    - Add BookOverview component with a title section! 📝
    
    Update layout to include Header and render BookList and BookOverview! 🔄
    
    Enhance styling in globals.css for better UI consistency! 🎨
    
    Add Prettier and Tailwind CSS plugin for improved code formatting! ✨

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,12 @@
+import BookList from '@/components/BookList';
+import BookOverview from '@/components/BookOverview';
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
-  return <Button>Click me</Button>;
+  return (
+    <>
+      <BookOverview />
+      <BookList />
+    </>
+  );
 }
---
+++ components/BookOverview.tsx
@@ -0,0 +1,9 @@
+export default function BookOverview() {
+  return (
+    <section className="book-overview">
+      <div className="flex flex-1 flex-col gap-5">
+        <h1>A long boog titleA long boog titleA long boog titleA long boog titleA long boog title</h1>
+      </div>
+    </section>
+  );
+}
