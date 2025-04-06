253945fdca7eca485fc4162dae69c52bd2798d6f
Netanel Mizrahi <nmiz1987@gmail.com>
2025-04-06 17:53:02 +0300

    ✨ Update Home page to display book details! 📚 - Integrate sample book data into BookOverview and BookList components! 🎉 - Enhance BookList component structure for better readability! 🛠️ - Add navigation links and sample books to constants for easy access! 📖 - Refine styles for BookOverview component! 🎨

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,3 +1,7 @@
 export default function BookList() {
-  return <div>BookList</div>;
+  return (
+    <section>
+      <h2 className="font-bebas-neue text-4xl text-light-100">Popular books</h2>
+    </section>
+  );
 }
