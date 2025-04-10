80553f07abe84f36749bf2ee509e04c9d5f2907f
Netanel Mizrahi <nmiz1987@gmail.com>
2025-04-10 15:25:51 +0300

    ✨ Set up database configuration and schema for user management! 🎉 - Added `drizzle.config.ts` for database configuration with PostgreSQL. - Created `drizzle.ts` to initialize the database connection using `drizzle-orm`. - Defined user schema in `schema.ts` with relevant fields and enums for status and roles. - Implemented initial migration files for creating the users table and associated types. - Updated `package.json` and `package-lock.json` to include new dependencies for database handling.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,13 @@ import BookList from '@/components/BookList';
 import BookOverview from '@/components/BookOverview';
 import { Button } from '@/components/ui/button';
 import { sampleBooks } from '@/constants';
+import { db } from '@/database/drizzle';
+import { usersTable } from '@/database/schema';
 
-export default function Home() {
+export default async function Home() {
+  const result = await db.select().from(usersTable);
+  console.log(JSON.stringify(result, null, 2));
+
   return (
     <>
       <BookOverview {...sampleBooks[0]} />
